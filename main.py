14077d6fd3728d87f86cb24ec8f81d7b9a2ab892
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-07-01 12:16:57 -0400

    Rename variables to be more precise/clear

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from models import JobPosting
 
 BASE_URL = "https://ca.indeed.com"
 JOB_SEARCH_PAGE = rf"{BASE_URL}/jobs?q=front+end+developer&l=Toronto%2C+ON&radius=50"
-JOB_ID_PARAM = "&vjk=[a-zA-Z0-9]+"
+JOB_ID_PARAM_PATTERN = "&vjk=[a-zA-Z0-9]+"
 
 
 def run(p: Playwright) -> None:
@@ -18,7 +18,7 @@ def run(p: Playwright) -> None:
     page.goto(JOB_SEARCH_PAGE)
 
     # a client-side redirect occurs where the job id of the first listed job is appended as a query param of "vjk"
-    expect(page).to_have_url(re.compile(f"{re.escape(JOB_SEARCH_PAGE)}{JOB_ID_PARAM}"))
+    expect(page).to_have_url(re.compile(f"{re.escape(JOB_SEARCH_PAGE)}{JOB_ID_PARAM_PATTERN}"))
 
     filer_by_date_posted(page)
     page.get_by_label("close", exact=True).click()  # close subscribe to updates modal
@@ -27,11 +27,11 @@ def run(p: Playwright) -> None:
     job_link_elements = page.get_by_role("button", name=re.compile("^full details of.*", re.IGNORECASE)).all()
     job_postings: list[JobPosting] = []
 
-    for link in job_link_elements:
-        job_title = link.text_content()
+    for link_element in job_link_elements:
+        job_title = link_element.text_content()
 
         if re.search(r"(?i)front[-\s]?end", job_title):
-            job_postings.append(JobPosting(title=job_title, url=link.get_attribute("href")))
+            job_postings.append(JobPosting(title=job_title, url=link_element.get_attribute("href")))
             continue
 
         # TODO: check for contents within posting to see if if keywords, "React", "Javascript", "Typescript", etc. exist
